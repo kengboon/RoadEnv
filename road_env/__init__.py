fcc0f6ad912811a5d4c44f0541e42b870a8ea15f
KB Teo <kengboon@users.noreply.github.com>
2023-05-04 09:22:29 +0800

    Add the code from highway_env

---
+++ road_env/__init__.py
@@ -0,0 +1,11 @@
+# Hide Pygame support prompt
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
+
+from gymnasium.envs.registration import register
+
+def register_road_envs():
+    register(
+        id='urban-road-v0',
+        entry_point='road_env.envs.UrbanRoadEnv',
+        )
