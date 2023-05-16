20e8aa3096320f81394af9fcef1a8f209bcbdcc5
KB Teo <kengboon@users.noreply.github.com>
2023-05-16 13:23:43 +0800

    Add hidden_dim to constructor argument

--- rl_algorithms/networks/rac.py
+++ rl_algorithms/networks/rac.py
@@ -1,10 +1,10 @@
 import torch
 import torch.nn as nn
-hidden_dim = 128
 
 class RecurrentActor(nn.Module):
-    def __init__(self, state_dim, action_dim):
+    def __init__(self, state_dim, action_dim, hidden_dim):
         super().__init__()
+        self.hidden_dim = hidden_dim
         self.lstm = nn.LSTMCell(state_dim, hidden_dim)
         self.fc = nn.Linear(hidden_dim, action_dim)
 
@@ -16,8 +16,8 @@ class RecurrentActor(nn.Module):
 class RecurrentCritic(nn.Module):
     def __init__(self, state_dim, action_dim):
         super().__init__()
-        self.lstm = nn.LSTMCell(state_dim + action_dim, hidden_dim)
-        self.fc = nn.Linear(hidden_dim, 1)
+        self.lstm = nn.LSTMCell(state_dim + action_dim, self.hidden_dim)
+        self.fc = nn.Linear(self.hidden_dim, 1)
 
     def forward(self, state, action, hidden):
         x = torch.cat([state, action], dim=1)
