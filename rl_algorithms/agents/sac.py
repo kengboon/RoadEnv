c7cd4bdd7a8ba2ade0653d189b9303f3d8e5a966
KB Teo <kengboon@users.noreply.github.com>
2023-05-17 17:23:27 +0800

    Fix variable names

--- rl_algorithms/agents/sac.py
+++ rl_algorithms/agents/sac.py
@@ -19,25 +19,27 @@ class SACAgent:
             critic_2,
             critic_1_target,
             critic_2_target,
+            gamma=.99,
             buffer_size=100000,
             batch_size=1,
             alpha=0.1,
             recurrent=False
         ):
         self.alpha = alpha
+        self.gamma = gamma
         self.buffer_size = buffer_size
         self.batch_size = batch_size
         self.replay_buffer = deque(maxlen=self.buffer_size)
         self.actor = actor
-        self.actor_optimizer = optim.Adam(self.actor.parameters(), lr=0.0003)
+        self.optimizer_actor = optim.Adam(self.actor.parameters(), lr=0.0003)
 
-        self.critic_1 = critic_1
-        self.critic_1_optimizer = optim.Adam(self.critic_1.parameters(), lr=0.0003)
-        self.critic_1_target = critic_1_target
+        self.critic1 = critic_1
+        self.optimizer_critic1 = optim.Adam(self.critic1.parameters(), lr=0.0003)
+        self.target_critic1 = critic_1_target
 
-        self.critic_2 = critic_2
-        self.critic_2_optimizer = optim.Adam(self.critic_2.parameters(), lr=0.0003)
-        self.critic_2_target = critic_2_target
+        self.critic2 = critic_2
+        self.optimizer_critic2 = optim.Adam(self.critic2.parameters(), lr=0.0003)
+        self.target_critic2 = critic_2_target
 
         self.target_entropy = -action_dim
         self.log_alpha = torch.zeros(1, requires_grad=True, device=device)
@@ -78,8 +80,8 @@ class SACAgent:
             next_action_dist = Normal(next_action_mean, next_action_std)
             next_action = next_action_dist.sample()
             next_log_prob = next_action_dist.log_prob(next_action)
-            target_q1 = self.critic_1_target(next_state, next_action)
-            target_q2 = self.critic_2_target(next_state, next_action)
+            target_q1 = self.target_critic1(next_state, next_action)
+            target_q2 = self.target_critic2(next_state, next_action)
             target_q = torch.min(target_q1, target_q2) - self.alpha * next_log_prob
             target_q = reward + (1 - done) * self.gamma * target_q
 
@@ -107,10 +109,10 @@ class SACAgent:
         actor_loss.backward()
         self.optimizer_actor.step()
 
-        for param, target_param in zip(self.critic_1.parameters(), self.critic_1_target.parameters()):
+        for param, target_param in zip(self.critic1.parameters(), self.target_critic1.parameters()):
             target_param.data.copy_(tau * param.data + (1 - tau) * target_param.data)
 
-        for param, target_param in zip(self.critic_2.parameters(), self.critic_2_target.parameters()):
+        for param, target_param in zip(self.critic2.parameters(), self.target_critic2.parameters()):
             target_param.data.copy_(tau * param.data + (1 - tau) * target_param.data)
 
     def _sample_next_action_log_prob(self, next_state):
