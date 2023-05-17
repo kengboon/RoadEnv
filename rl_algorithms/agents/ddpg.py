37074f87ff115b86f2f57383f4abad40d93b1e5a
KB Teo <kengboon@users.noreply.github.com>
2023-05-17 11:52:10 +0800

    Handle recurrent network

--- rl_algorithms/agents/ddpg.py
+++ rl_algorithms/agents/ddpg.py
@@ -20,7 +20,8 @@ class DDPGAgent:
             gamma=.99,
             tau=0.001,
             buffer_size=100000,
-            batch_size=64
+            batch_size=1,
+            recurrent=False
         ):
         self.actor = actor
         self.actor_target = actor_target
@@ -34,13 +35,21 @@ class DDPGAgent:
         self.gamma = gamma
         self.tau = tau
         self.max_action = max_action
-        self.hx = torch.zeros(1, hidden_size)
-        self.cx = torch.zeros(1, hidden_size)
+        self.recurrent = recurrent
+        if self.recurrent:
+            hx = torch.zeros(self.batch_size, hidden_size)
+            cx = torch.zeros(self.batch_size, hidden_size)
+            self.hidden_state = (hx, cx)
 
     def get_action(self, state):
-        state = torch.FloatTensor(state)
-        action = self.actor(state.unsqueeze(0)).squeeze(0)
-        return action.detach().numpy()
+        state = torch.FloatTensor(state).unsqueeze(0)
+        with torch.no_grad():
+            if self.recurrent:
+                action, self.hidden_state = self.actor(state, self.hidden_state)
+            else:
+                action = self.actor(state)
+        action = action.squeeze(0).detach().numpy()
+        return action
     
     def update(self):
         if len(self.replay_buffer) < self.batch_size:
@@ -56,9 +65,16 @@ class DDPGAgent:
         done_batch = torch.FloatTensor(done_batch)
 
         # Update critic
-        Q_values = self.critic(state_batch, action_batch)
-        next_actions = self.actor_target(next_state_batch)
-        next_Q_values = self.critic_target(next_state_batch, next_actions.detach())
+        if self.recurrent:
+            Q_values, _ = self.critic(state_batch, action_batch, self.hidden_state)
+            with torch.no_grad():
+                next_actions, _ = self.actor_target(next_state_batch, self.hidden_state)
+                next_Q_values, _ = self.critic_target(next_state_batch, next_actions, self.hidden_state)
+        else:
+            Q_values = self.critic(state_batch, action_batch)
+            with torch.no_grad():
+                next_actions = self.actor_target(next_state_batch)
+                next_Q_values = self.critic_target(next_state_batch, next_actions)
         target_Q_values = reward_batch + (1 - done_batch) * self.gamma * next_Q_values
         critic_loss = F.mse_loss(Q_values, target_Q_values.unsqueeze(1))
 
@@ -67,8 +83,12 @@ class DDPGAgent:
         self.critic_optimizer.step()
 
         # Update actor
-        actions = self.actor(state_batch)
-        actor_loss = -self.critic(state_batch, actions).mean()
+        if self.recurrent:
+            actions, _ = self.actor(state_batch, self.hidden_state)
+            actor_loss = -self.critic(state_batch, actions, self.hidden_state)[0].mean()
+        else:
+            actions = self.actor(state_batch)
+            actor_loss = -self.critic(state_batch, actions).mean()
 
         self.actor_optimizer.zero_grad()
         actor_loss.backward()
