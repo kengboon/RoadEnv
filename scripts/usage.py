b7affbc3d11aa85c680bc1da65bbd942b0d20960
KB Teo <kengboon@users.noreply.github.com>
2023-06-07 10:56:01 +0800

    Track usage script

---
+++ scripts/usage.py
@@ -0,0 +1,59 @@
+# %% Working directory
+import sys, os
+print('Working dir:', os.getcwd())
+# In order to import modules from directories
+if os.path.exists('road_env'):
+    sys.path.append('.')
+else:
+    sys.path.append('..')
+
+# %% Import
+import gymnasium as gym
+import matplotlib.pyplot as plt
+
+# %% Register environment
+from road_env import register_road_envs
+register_road_envs()
+
+# %% Print EnvSpec
+#print(gym.spec('urban-road-v0'))
+
+# %% Make environment
+env = gym.make('urban-road-v0', render_mode='rgb_array')
+
+
+print('# Action space ', env.action_space.shape)
+print(env.action_space.sample())
+
+print('# Observation space', env.observation_space.shape)
+print(env.observation_space.sample())
+
+env.configure({
+    "random_seed": 10,
+    "duration": 60, # Maximum duration (s) per episode
+})
+env.reset()
+
+# %% Show display
+plt.imshow(env.render())
+
+# %% Execution
+num_episodes = 20
+for episode in range(num_episodes):
+    print('Episode', episode+1)
+    episode_reward = 0
+
+    while True: # Use config["duration"] to truncate
+        action = env.action_space.sample()
+        obs, reward, done, truncated, info = env.step(action)
+        env.render() # Note: Do not render during training
+
+        episode_reward += reward
+
+        if done or truncated:
+            obs, info = env.reset()
+            break
+
+    print('Reward', episode_reward)
+
+env.close()
