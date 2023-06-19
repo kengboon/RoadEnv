faf49e6fae0ed5f46275b1c495ab3ef990a36348
KB Teo <kengboon@users.noreply.github.com>
2023-06-19 10:43:33 +0800

    Fix and flatten observation

--- rl_algorithms/agents/ddpg.py
+++ rl_algorithms/agents/ddpg.py
@@ -1,8 +1,11 @@
 from collections import deque
 import random
+import numpy as np
 import torch
+from torch.autograd import Variable
 import torch.nn.functional as F
 import torch.optim as optim
+from ..networks import *
 
 class DDPGAgent:
     def __init__(
@@ -11,22 +14,24 @@ class DDPGAgent:
             action_dim,
             max_action,
             hidden_size,
-            actor,
-            actor_target,
-            critic,
-            critic_target,
             lr_actor=0.001,
             lr_critic=0.001,
             gamma=.99,
             tau=0.001,
-            buffer_size=100000,
+            buffer_size=10000,
             batch_size=1,
             recurrent=False
         ):
-        self.actor = actor
-        self.actor_target = actor_target
-        self.critic = critic
-        self.critic_target = critic_target
+        if recurrent:
+            self.actor = RecurrentActor(state_dim, action_dim, max_action)
+            self.actor_target = RecurrentActor(state_dim, action_dim, max_action)
+            self.critic = RecurrentCritic(state_dim, action_dim)
+            self.critic_target = RecurrentCritic(state_dim, action_dim)
+        else:
+            self.actor = Actor(state_dim, action_dim, max_action)
+            self.actor_target = Actor(state_dim, action_dim, max_action)
+            self.critic = Critic(state_dim, action_dim)
+            self.critic_target = Critic(state_dim, action_dim)
         self.actor_optimizer = optim.Adam(self.actor.parameters(), lr=lr_actor)
         self.critic_optimizer = optim.Adam(self.critic.parameters(), lr=lr_critic)
         self.buffer_size = buffer_size
@@ -34,21 +39,23 @@ class DDPGAgent:
         self.replay_buffer = deque(maxlen=self.buffer_size)
         self.gamma = gamma
         self.tau = tau
-        self.max_action = max_action
         self.recurrent = recurrent
         if self.recurrent:
             hx = torch.zeros(self.batch_size, hidden_size)
             cx = torch.zeros(self.batch_size, hidden_size)
             self.hidden_state = (hx, cx)
 
+    def flatten_state(self, state):
+        return state.flatten()
+
     def get_action(self, state):
-        state = torch.FloatTensor(state).unsqueeze(0)
+        state = Variable(torch.from_numpy(self.flatten_state(state)).float())
         with torch.no_grad():
             if self.recurrent:
                 action, self.hidden_state = self.actor(state, self.hidden_state)
             else:
                 action = self.actor(state)
-        action = action.squeeze(0).detach().numpy()
+        action = action.detach().numpy()
         return action
     
     def update(self):
@@ -58,11 +65,11 @@ class DDPGAgent:
         batch = random.sample(self.replay_buffer, self.batch_size)
         state_batch, action_batch, reward_batch, next_state_batch, done_batch = zip(*batch)
 
-        state_batch = torch.FloatTensor(state_batch)
-        action_batch = torch.FloatTensor(action_batch)
-        reward_batch = torch.FloatTensor(reward_batch)
-        next_state_batch = torch.FloatTensor(next_state_batch)
-        done_batch = torch.FloatTensor(done_batch)
+        state_batch = Variable(torch.from_numpy(np.array(state_batch)).float())
+        action_batch = Variable(torch.from_numpy(np.array(action_batch)).float())
+        reward_batch = Variable(torch.from_numpy(np.array(reward_batch)).float())
+        next_state_batch = Variable(torch.from_numpy(np.array(next_state_batch)).float())
+        done_batch = Variable(torch.from_numpy(np.array(done_batch)).float())
 
         # Update critic
         if self.recurrent:
@@ -76,7 +83,7 @@ class DDPGAgent:
                 next_actions = self.actor_target(next_state_batch)
                 next_Q_values = self.critic_target(next_state_batch, next_actions)
         target_Q_values = reward_batch + (1 - done_batch) * self.gamma * next_Q_values
-        critic_loss = F.mse_loss(Q_values, target_Q_values.unsqueeze(1))
+        critic_loss = F.mse_loss(Q_values, target_Q_values)
 
         self.critic_optimizer.zero_grad()
         critic_loss.backward()
@@ -102,4 +109,4 @@ class DDPGAgent:
             target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
 
     def add_to_replay_buffer(self, state, action, reward, next_state, done):
-        self.replay_buffer.append((state, action, reward, next_state, done))
+        self.replay_buffer.append((self.flatten_state(state), action, reward, self.flatten_state(next_state), done))
