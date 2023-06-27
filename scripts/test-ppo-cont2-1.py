e5c88f48c57675378c3a6e921e6acff9555511f7
KB Teo <kengboon@users.noreply.github.com>
2023-06-27 19:37:41 +0800

    Add test code for PPO

---
+++ scripts/test-ppo-cont2-1.py
@@ -0,0 +1,82 @@
+# %% Working directory
+import sys, os
+if os.path.exists("road_env"):
+    sys.path.append(".")
+else:
+    sys.path.append("..")
+
+# %% Register environment
+from road_env import register_road_envs
+register_road_envs()
+
+# %% Make environment
+import gymnasium as gym
+env = gym.make("urban-road-v0", render_mode="rgb_array")
+env.configure({
+    "random_seed": None,
+    "duration": 999,
+    "obstacle_preset": 4
+})
+#env.config["obstacle_count"] = 0
+#env.config["pedestrians"]["count"] = 0
+
+# %% Get dimensions
+print('Observation space', env.observation_space.shape)
+print('Action shape', env.action_space.shape)
+
+state_dim = env.observation_space.shape[0]
+action_dim = env.action_space.shape[0]
+max_action = env.action_space.high[0]
+print("State dim:", state_dim, "Action dim:", action_dim, "Max action:", max_action)
+
+# %% Make DRL Trainer
+hidden_dim = 512
+
+from rl_algorithms2.ppo_continuous2 import PPO
+ppo = PPO(
+    state_dim=state_dim,
+    action_dim=action_dim,
+    action_range=max_action,
+    hidden_dim=hidden_dim,
+)
+
+from datetime import datetime
+model_type = "ppo_cont2"
+train_id = "230627174457"
+episode = "500"
+model_dir = "models/" + model_type + "-" + train_id + "/" + episode
+
+# %% Testing
+obs, info = env.reset()
+done = truncated = False
+num_episode = 20
+for episode in range(num_episode):
+    print('Episode', episode+1)
+
+    num_steps = 0
+    episode_reward = 0
+
+    while True: # Use config["duration"] to truncate
+        action = ppo.choose_action(obs)
+        print(action)
+
+        next_obs, reward, done, truncated, info = env.step(action)
+
+        obs = next_obs
+        num_steps += 1
+        episode_reward += reward
+        env.render() # Note: Do not render during training
+
+        if done or truncated:
+            obs, info = env.reset()
+            break
+
+
+    episode_log = {
+        "Episode": episode+1,
+        "Time steps": num_steps,
+        "Episode Rewards": episode_reward,
+        "Average Rewards": episode_reward / num_steps,
+    }
+    print(episode_log)
+env.close()
