29c35daee55e45fbc7150434433359e7f9b12610
KB Teo <kengboon@users.noreply.github.com>
2023-07-04 10:35:44 +0800

    Add map_location

--- rl_algorithms2/sac_v2_lstm.py
+++ rl_algorithms2/sac_v2_lstm.py
@@ -132,9 +132,9 @@ class SAC_Trainer():
         torch.save(self.policy_net.state_dict(), path+'_policy')
 
     def load_model(self, path):
-        self.soft_q_net1.load_state_dict(torch.load(path+'_q1'))
-        self.soft_q_net2.load_state_dict(torch.load(path+'_q2'))
-        self.policy_net.load_state_dict(torch.load(path+'_policy'))
+        self.soft_q_net1.load_state_dict(torch.load(path+'_q1', map_location=torch.device(device)))
+        self.soft_q_net2.load_state_dict(torch.load(path+'_q2', map_location=torch.device(device)))
+        self.policy_net.load_state_dict(torch.load(path+'_policy', map_location=torch.device(device)))
 
         self.soft_q_net1.eval()
         self.soft_q_net2.eval()
