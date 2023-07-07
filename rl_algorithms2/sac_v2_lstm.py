016e70005c31f4f84a8e646051c7079b15859739
KB Teo <kengboon@users.noreply.github.com>
2023-07-07 14:25:18 +0800

    Move the code of networks

--- rl_algorithms2/sac_v2_lstm.py
+++ rl_algorithms2/sac_v2_lstm.py
@@ -14,8 +14,9 @@ import torch.optim as optim
 import torch.nn.functional as F
 from torch.distributions import Normal
 from .common.buffers import *
-from .common.value_networks import *
-from .common.policy_networks import *
+from .common.initialize import linear_weights_init
+from .common.value_networks import QNetworkBase
+from .common.policy_networks import PolicyNetworkBase
 
 GPU = True
 device_idx = 0
@@ -24,6 +25,132 @@ if GPU:
 else:
     device = torch.device("cpu")
 print(device)
+
+class QNetworkLSTM(QNetworkBase):
+    """
+    Q network with LSTM structure.
+    The network follows two-branch structure as in paper: 
+    Sim-to-Real Transfer of Robotic Control with Dynamics Randomization
+    """
+    def __init__(self, state_space, action_space, hidden_dim, activation=F.relu, output_activation=None):
+        super().__init__(state_space, action_space, activation)
+        self.hidden_dim = hidden_dim
+
+        self.linear1 = nn.Linear(self._state_dim+self._action_dim, hidden_dim)
+        self.linear2 = nn.Linear(self._state_dim+self._action_dim, hidden_dim)
+        self.lstm1 = nn.LSTM(hidden_dim, hidden_dim)
+        self.linear3 = nn.Linear(2*hidden_dim, hidden_dim)
+        self.linear4 = nn.Linear(hidden_dim, 1)
+        # weights initialization
+        self.linear4.apply(linear_weights_init)
+        
+    def forward(self, state, action, last_action, hidden_in):
+        """ 
+        state shape: (batch_size, sequence_length, state_dim)
+        output shape: (batch_size, sequence_length, 1)
+        for lstm needs to be permuted as: (sequence_length, batch_size, state_dim)
+        """
+        state = state.permute(1,0,2)
+        action = action.permute(1,0,2)
+        last_action = last_action.permute(1,0,2)
+        # branch 1
+        fc_branch = torch.cat([state, action], -1) 
+        fc_branch = self.activation(self.linear1(fc_branch))
+        # branch 2
+        lstm_branch = torch.cat([state, last_action], -1) 
+        lstm_branch = self.activation(self.linear2(lstm_branch))  # linear layer for 3d input only applied on the last dim
+        lstm_branch, lstm_hidden = self.lstm1(lstm_branch, hidden_in)  # no activation after lstm
+        # merged
+        merged_branch=torch.cat([fc_branch, lstm_branch], -1) 
+
+        x = self.activation(self.linear3(merged_branch))
+        x = self.linear4(x)
+        x = x.permute(1,0,2)  # back to same axes as input    
+        return x, lstm_hidden    # lstm_hidden is actually tuple: (hidden, cell)
+    
+class SAC_PolicyNetworkLSTM(PolicyNetworkBase):
+    def __init__(self, state_space, action_space, hidden_size, action_range=1., init_w=3e-3, log_std_min=-20, log_std_max=2):
+        super().__init__(state_space, action_space, action_range=action_range)
+        
+        self.log_std_min = log_std_min
+        self.log_std_max = log_std_max
+        self.hidden_size = hidden_size
+        
+        self.linear1 = nn.Linear(self._state_dim, hidden_size)
+        self.linear2 = nn.Linear(self._state_dim+self._action_dim, hidden_size)
+        self.lstm1 = nn.LSTM(hidden_size, hidden_size)
+        self.linear3 = nn.Linear(2*hidden_size, hidden_size)
+        self.linear4 = nn.Linear(hidden_size, hidden_size)
+
+        self.mean_linear = nn.Linear(hidden_size, self._action_dim)
+        self.mean_linear.weight.data.uniform_(-init_w, init_w)
+        self.mean_linear.bias.data.uniform_(-init_w, init_w)
+        
+        self.log_std_linear = nn.Linear(hidden_size, self._action_dim)
+        self.log_std_linear.weight.data.uniform_(-init_w, init_w)
+        self.log_std_linear.bias.data.uniform_(-init_w, init_w)
+
+
+    def forward(self, state, last_action, hidden_in):
+        """ 
+        state shape: (batch_size, sequence_length, state_dim)
+        output shape: (batch_size, sequence_length, action_dim)
+        for lstm needs to be permuted as: (sequence_length, batch_size, -1)
+        """
+        state = state.permute(1,0,2)
+        last_action = last_action.permute(1,0,2)
+        # branch 1
+        fc_branch = F.relu(self.linear1(state))
+        # branch 2
+        lstm_branch = torch.cat([state, last_action], -1)
+        lstm_branch = F.relu(self.linear2(lstm_branch))
+        lstm_branch, lstm_hidden = self.lstm1(lstm_branch, hidden_in)  # no activation after lstm
+        # merged
+        merged_branch=torch.cat([fc_branch, lstm_branch], -1) 
+        x = F.relu(self.linear3(merged_branch))
+        x = F.relu(self.linear4(x))
+        x = x.permute(1,0,2)  # permute back
+
+        mean    = self.mean_linear(x)
+        # mean    = F.leaky_relu(self.mean_linear(x))
+        log_std = self.log_std_linear(x)
+        log_std = torch.clamp(log_std, self.log_std_min, self.log_std_max)
+        
+        return mean, log_std, lstm_hidden
+    
+    def evaluate(self, state, last_action, hidden_in, epsilon=1e-6):
+        '''
+        generate sampled action with state as input wrt the policy network;
+        '''
+        mean, log_std, hidden_out = self.forward(state, last_action, hidden_in)
+        std = log_std.exp() # no clip in evaluation, clip affects gradients flow
+        
+        normal = Normal(0, 1)
+        z = normal.sample(mean.shape)
+        action_0 = torch.tanh(mean + std * z.to(device))  # TanhNormal distribution as actions; reparameterization trick
+        action = self.action_range * action_0
+        log_prob = Normal(mean, std).log_prob(mean + std * z.to(device)) - torch.log(
+            1. - action_0.pow(2) + epsilon) - np.log(self.action_range)
+        # both dims of normal.log_prob and -log(1-a**2) are (N,dim_of_action);
+        # the Normal.log_prob outputs the same dim of input features instead of 1 dim probability,
+        # needs sum up across the features dim to get 1 dim prob; or else use Multivariate Normal.
+        log_prob = log_prob.sum(dim=-1, keepdim=True)
+        return action, log_prob, z, mean, log_std, hidden_out
+
+    def get_action(self, state, last_action, hidden_in, deterministic=True):
+        state = torch.FloatTensor(state).unsqueeze(0).unsqueeze(0).to(device)  # increase 2 dims to match with training data
+        last_action = torch.FloatTensor(last_action).unsqueeze(0).unsqueeze(0).to(device)
+        mean, log_std, hidden_out = self.forward(state, last_action, hidden_in)
+        std = log_std.exp()
+        
+        normal = Normal(0, 1)
+        z = normal.sample(mean.shape).to(device)
+        action = self.action_range * torch.tanh(mean + std * z)
+
+        action = self.action_range * torch.tanh(mean).detach().cpu().numpy() if deterministic else \
+        action.detach().cpu().numpy()
+        return action[0][0], hidden_out
+
 
 class SAC_Trainer():
     def __init__(self, replay_buffer, state_space, action_space, hidden_dim, action_range):
